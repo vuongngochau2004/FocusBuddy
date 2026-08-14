b6ac88b7bf128b2943096563e9d0e66326c7b65d
dut-anhtu24 <quedinhanhtu@gmail.com>
2026-08-14 09:19:28 +0700

    feat: Implement Academic Management Module

--- be/app/api/router.py
+++ be/app/api/router.py
@@ -3,7 +3,18 @@ from fastapi import APIRouter
 api_router = APIRouter()
 
 from app.api.v1.user import router as user_router
+from app.api.v1.universities import router as university_router
+from app.api.v1.majors import router as major_router
+from app.api.v1.curriculums import router as curriculum_router
+from app.api.v1.courses import router as course_router
+from app.api.v1.academic_terms import router as academic_term_router
+
 api_router.include_router(user_router, prefix="/v1/users", tags=["Users"])
+api_router.include_router(university_router, prefix="/v1/universities", tags=["Universities"])
+api_router.include_router(major_router, prefix="/v1/majors", tags=["Majors"])
+api_router.include_router(curriculum_router, prefix="/v1/curriculums", tags=["Curriculums"])
+api_router.include_router(course_router, prefix="/v1/courses", tags=["Courses"])
+api_router.include_router(academic_term_router, prefix="/v1/academic-terms", tags=["Academic Terms"])
 
 @api_router.get("/v1/ping", tags=["Health Check"])
 async def ping():
---
+++ be/docs/migration_guide.md
@@ -0,0 +1,6 @@
+# HƯỚNG DẪN MIGRATION CODE FIRST CREATE TABLE FOR DATABASE
+
+## B1: Tạo DB trong postgreSQL, đặt tên tùy ý (sửa đường dẫn trong môi trường thành tên db của bạn). 
+### LƯU Ý: chú ý port postgreSQL đang dùng là gì (5433 hay 5432).Vào pGAmin --> Server --> PostgreSQL --> Properties--> Connection 
+
+## B2: vào terminal trong vs code, 
